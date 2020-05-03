75081a3e5bf7f7056032cc07d7235695fb41acf8
mutohiroshi <mutohiroshi@team-lab.com>
2020-05-03 17:07:19 +0900

    create: create ListItem component

---
+++ components/ListItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const ListItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: 'https://picsum.photos/seed/picsum/200/300' }}
+        />
+      </View>
+      <View style={styles.rigthtContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          The API will return 30 items per page by default.
+          To request another page, use the ?page parameter.
+          To change the amount of items per page, use the ?limit parameter.
+        </Text>
+        <Text style={styles.subText}>
+          React News
+        </Text>
+      </View>
+    </View>
+  )
+}
+export default ListItem
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row'
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100
+  },
+  rigthtContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 16
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray'
+  }
+});
