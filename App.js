caf4e18d4ad264949222dc4e6e7feda362b92d48
mutohiroshi <mutohiroshi@team-lab.com>
2020-05-03 21:19:42 +0900

    create: create news list view by using FlatList

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem'
+import articles from './dummies/articles'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListItem
-        imageUrl="https://picsum.photos/seed/picsum/200/300" 
-        title="The API will return 30 items per page by default.
-        To request another page, use the ?page parameter.
-        To change the amount of items per page, use the ?limit parameter."
-        author="sample news"
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({item}) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   }
 });
